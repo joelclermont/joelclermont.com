17d197c649a7ed9e60f267b25497134eef55b197
Joel Clermont <jclermont@gmail.com>
2017-12-25 11:49:45 -0600

    set up contact form

---
+++ content/contact.md
@@ -0,0 +1,10 @@
+---
+title: "Contact"
+date: 2017-12-25T11:24:02-06:00
+layout: "about"
+comments: false
+---
+<div id="wufoo-z7x3p3">
+Fill out my <a href="http://jclermont.wufoo.com/forms/z7x3p3">online form</a>.
+</div>
+<script type="text/javascript" src="/contact-form.js"></script>
---
+++ static/contact-form.js
@@ -0,0 +1,14 @@
+var z7x3p3;(function(d, t) {
+    var s = d.createElement(t), options = {
+    'userName':'jclermont', 
+    'formHash':'z7x3p3', 
+    'autoResize':true,
+    'height':'497',
+    'async':true,
+    'header':'show'};
+    s.src = ('https:' == d.location.protocol ? 'https://' : 'http://') + 'wufoo.com/scripts/embed/form.js';
+    s.onload = s.onreadystatechange = function() {
+    var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return;
+    try { z7x3p3 = new WufooForm();z7x3p3.initialize(options);z7x3p3.display(); } catch (e) {}};
+    var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr);
+    })(document, 'script');
